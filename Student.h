cbfa0eb3c9013fcee2bf27a98ce7c144989c1eee
Kamel R <kamel.r@wayne.edu>
2023-01-11 18:56:58 -0500

    started adding members to the student class

---
+++ Student.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+using namespace std; 
+
+class Student
+{ // members 
+private: 
+	string name; 
+	string id;
+	string major;
+
+// functions 
+public: 
+	// set name 
+	void setName(string name); 
+	// get name
+	string getName();
+
+
+
+
+
+
+
+};
+
